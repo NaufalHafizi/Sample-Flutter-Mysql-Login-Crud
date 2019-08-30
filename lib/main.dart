35edfb995e103aa65acb4919ad976e0a669c83b0
Naufal Hafizi <naufalhafizi@gmail.com>
2019-08-31 01:10:56 +0800

    new

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,7 @@ import 'package:mysqlcrudnlogin/ui/6_TeacherHomePage.dart';
 import 'package:provider/provider.dart';
 import 'model/getLogin.dart';
 
-//String phone = 'http://192.168.43.58';
-//String phone = 'http://10.0.2.2';
-String phone = 'https://www.naufalhafizi.my';
+String phone = 'yourURLorIp';
 
 void main() {
   runApp(
