65cda314954d395ec4940e00f65087e60ab58566
Naufal Hafizi <naufalhafizi@gmail.com>
2019-11-06 18:09:26 +0800

    camera

--- adddata.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include 'conn.php';
-	
-	$itemEmail = $_POST['email'];
-	$itemPassword = $_POST['password'];
-	$sekolah = $_POST['sekolah'];
-	
-	$connect->query("INSERT INTO user (Teacher_email,Teacher_password,Teacher_sekolah) VALUES ('".$itemEmail."','".$itemPassword."','".$sekolah."')")
-?>
--- deleteData.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    include 'conn.php';
-    $id = $_POST['ID'];
-    $connect->query("DELETE FROM user WHERE ID=".$id);
-
-?>
